ab504437ed3597e6d81bf8ee7edff7e683493a4a
Sameer Khan <contact@mdsameer.com.np>
2021-05-30 21:21:57 +0545

    feat(snowpack): damn this is so cool⚡.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import { render } from 'react-dom'
 import App from './App'
 
 render(<App />, document.getElementById('mount'))
+
+// Below code is a magic, only replace in production.
+if (import.meta.hot) {
+  import.meta.hot.accept()
+}
